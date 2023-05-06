c47c1d6bf81dab6a02e085cd5084bb97a81becc9
Lian Salmerón López <lembajosa@gmail.com>
2023-05-06 22:33:23 +0200

    Obtención de x recetas + paginacion

--- ETEN/app/Http/Controllers/IngredienteController.php
+++ ETEN/app/Http/Controllers/IngredienteController.php
@@ -14,15 +14,39 @@ class IngredienteController extends Controller
 
         $ingredientes = $request->ingredientes;
 
-        $queryWhere = "1=1";
-        foreach ($ingredientes as $ingrediente) {
-            $ingredienteS = "%".$ingrediente."%";
-            $queryWhere .= " AND (SELECT count(1) FROM ingredientes wHERE nombre_ingrediente LIKE '$ingredienteS' and id_receta=recetas.id) > 0";
+        $recetasTotales = Receta::get(['id', 'titulo', 'img']);
+        $tamanio = count($recetasTotales);
+
+        $nIngredientes = 12;
+        $offset = ($request->pagina - 1) * 12;
+
+        $nIngredientes = count($ingredientes);
+
+        if ($nIngredientes > 0) {
+
+            $queryWhere = "1=1";
+            foreach ($ingredientes as $ingrediente) {
+                $ingredienteS = "%" . $ingrediente . "%";
+                $queryWhere .= " AND (SELECT count(1) FROM ingredientes wHERE nombre_ingrediente LIKE '$ingredienteS' and id_receta=recetas.id) > 0";
+
+            }
+            $recetasResultados = DB::table('recetas')->whereRaw($queryWhere);
+
+            $tamanio = $recetasResultados->count();
+
+            if ($tamanio > 0) {
+                $recetas = $recetasResultados->select('id', 'titulo', 'img')->offset($offset)->limit(12)->get();
+
+                return [$recetas, $tamanio, sizeof($recetas)];
+            }
 
         }
-        $recetas = DB::table('recetas')->whereRaw($queryWhere)->get();
+        $tamanio = count($recetasTotales);
+        $nIngredientes = 0;
+        $recetas = Receta::select('id', 'titulo', 'img')->offset($offset)->limit(12)->get();
 
-        return json_encode($recetas);
+
+        return [$recetas, $tamanio, $nIngredientes];
 
     }
     public function obtenerIngredientes(Request $request){
